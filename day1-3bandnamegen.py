526b83cb3b25855fb6c83cbbac70512116d83242
Siddharth Rao <nutsforjs@gmail.com>
2022-11-05 11:18:32 +0530

    Day 1 assignments

---
+++ day1-3bandnamegen.py
@@ -0,0 +1,5 @@
+#Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+print("Welcome to the Band Name Generator.")
+city = input("What city did you grow up in?")
+pet = input("What is the name of your pet?")
+print("Your band name is: " + city + " " + pet)
